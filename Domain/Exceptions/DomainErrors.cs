1acfa3824b4362a27c2d310addf881e9015db08b
Ezequiel De La Rosa <e.delarosa@innovation.es>
2023-03-27 15:59:15 +0200

    Create a list of Domain Errors.

---
+++ Domain/Exceptions/DomainErrors.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Exceptions
+{
+    /// <summary>
+    /// Collection of Errors related to Domain.
+    /// </summary>
+    public static class DomainErrors
+    {
+        /// <summary>
+        /// Collection of Errors related to Person Entity.
+        /// </summary>
+        public static class Person
+        {
+            public static readonly Error AgeIsZeroOrLower = new Error("Person.CreatingPerson.", "Person can't have a age of zero or lower.");
+        }
+    }
+}
