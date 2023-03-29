edf0b3fbdba5929be54a848b9c71e70209002736
Ezequiel De La Rosa <91556738+dEzequiel@users.noreply.github.com>
2023-03-29 21:33:38 +0200

    Creation of database application context and code first model for Reception table.

---
+++ Infraestructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Infraestructure.Tables;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Context
+{
+    /// <summary>
+    /// Application database context.
+    /// </summary>
+    public class ApplicationDbContext :  DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        // Tables
+        public DbSet<ReceptionDocument> ReceptionsDocuments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ReceptionDocument>();
+        }
+    }
+}
---
+++ Infraestructure/Tables/ReceptionDocument.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Tables
+{
+    [Table("ReceptionDocument", Schema = "dbo")]
+    public class ReceptionDocument
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public Guid Category { get; private set; }
+        public int Sex { get; private set; }
+        public bool HasChip { get; private set; } 
+        public string Color { get; private set; } 
+        public string? Observations { get; private set; } 
+        public string? PickupLocation { get; private set; } 
+        public DateTime? PickupDate { get; private set; }
+    }
+}
