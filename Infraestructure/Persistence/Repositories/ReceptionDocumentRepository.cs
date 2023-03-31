14eb1bb49701cd6d1c17e122f27fc9182aa3dac0
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2023-03-31 11:15:44 +0200

    Install EntityFrameworkInMemoryDatabase and first ReceptionDocumentRepository test.

---
+++ Infraestructure/Persistence/Repositories/ReceptionDocumentRepository.cs
@@ -0,0 +1,49 @@
+﻿
+using Domain.Entities;
+using Domain.Interfaces.Repositories;
+using Infraestructure.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Persistence.Repositories
+{
+    public class ReceptionDocumentRepository : IReceptionDocumentRepository
+    {
+        protected readonly DbSet<ReceptionDocument> _receptions;
+
+        public ReceptionDocumentRepository(ApplicationDbContext context)
+        {
+            _receptions = context.Set<ReceptionDocument>();
+        }
+
+        public Task AddAsync(ReceptionDocument entity, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddRangeAsync(IEnumerable<ReceptionDocument> entities, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ReceptionDocument>> FindAsync(Expression<Func<ReceptionDocument, bool>> predicate, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<ReceptionDocument>> GetAllAsync(CancellationToken ct = default)
+        {
+            return await _receptions.ToListAsync();
+        }
+
+        public Task<ReceptionDocument?> GetAsync(Guid id, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Test/Infraestructure/ReceptionDocumentRepositoryTest.cs
@@ -0,0 +1,71 @@
+﻿using Domain.Enums;
+using Domain.Interfaces.Repositories;
+using Infraestructure.Context;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.InMemory;
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+using Infraestructure.Persistence.Repositories;
+
+namespace Test.Infraestructure
+{
+    [Collection("ReceptionDocumentRepository")]
+    public class ReceptionDocumentRepositoryTest
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IReceptionDocumentRepository repository;
+
+        /// <summary>
+        /// Constructor. XUnit constructor run before each test.
+        /// </summary>
+        public ReceptionDocumentRepositoryTest()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            _context = new ApplicationDbContext(options);
+            repository = new ReceptionDocumentRepository(_context);
+            Seed(_context);
+        }
+
+        [Fact]
+        public async void Should_Get_Set_Of_Objects()
+        {
+            // Act
+            var result = await repository.GetAllAsync();
+
+            // Assert
+            Assert.IsAssignableFrom<IList<ReceptionDocument>>(result);
+        }
+
+        private void Seed(ApplicationDbContext context)
+        {
+            var CategoryId = Guid.Parse("1d5f841d-e020-4192-a0eb-77e97eb1b7d4");
+
+            var receptionDocuments = new List<ReceptionDocument>();
+
+            var reception = ReceptionDocument.Create(
+                Guid.Parse("a06a5f34-58ac-41ec-bccb-9a8c38696bd2"),
+                Guid.Parse("a06a5f34-58ac-41ec-bccb-9a8c38696bd2"),
+                CategoryId,
+                ((int)Sex.Male),
+                false,
+                "black",
+                null,
+                null,
+                null
+           );
+
+            receptionDocuments.Add(reception.Value!);
+
+            context.ReceptionsDocuments.AddRange(receptionDocuments);
+            context.SaveChanges();
+        }
+    }
+}
