284149985ca19c17241c10c9e0c6e8e69c46d34d
Ezequiel De La Rosa <91556738+dEzequiel@users.noreply.github.com>
2023-03-31 23:06:59 +0200

    Implement Animal repository with domain repository contract. Add method documentation for ReceptionDocumentRepository.

---
+++ Infraestructure/Persistence/Repositories/AnimalRepository.cs
@@ -0,0 +1,55 @@
+﻿using Domain.Entities;
+using Domain.Interfaces.Repositories;
+using Infraestructure.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Infraestructure.Persistence.Repositories
+{
+    public class AnimalRepository : IAnimalRepository
+    {
+        /// <summary>
+        /// Attributes.
+        /// </summary>
+        protected readonly DbSet<Animal> _animals;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="context"></param>
+        public AnimalRepository(ApplicationDbContext context)
+        {
+            _animals = context.Set<Animal>();
+        }
+
+        /// <inheritdoc/>
+        public async Task AddAsync(Animal entity, CancellationToken ct = default)
+        {
+            await _animals.AddAsync(entity, ct);
+        }
+
+        /// <inheritdoc/>
+        public async Task AddRangeAsync(IEnumerable<Animal> entities, CancellationToken ct = default)
+        {
+            await _animals.AddRangeAsync(entities, ct);
+        }
+
+        /// <inheritdoc/>
+        public async Task<IEnumerable<Animal>> FindAsync(Expression<Func<Animal, bool>> predicate, CancellationToken ct = default)
+        {
+            return await _animals.AsNoTracking().Where(predicate).ToListAsync(ct);
+        }
+
+        /// <inheritdoc/>
+        public async Task<IEnumerable<Animal>> GetAllAsync(CancellationToken ct = default)
+        {
+            return await _animals.AsNoTracking().ToListAsync(ct);
+        }
+
+        /// <inheritdoc/>
+        public async Task<Animal?> GetAsync(Guid id, CancellationToken ct = default)
+        {
+            return await _animals.FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
--- Infraestructure/Persistence/Repositories/ReceptionDocumentRepository.cs
+++ Infraestructure/Persistence/Repositories/ReceptionDocumentRepository.cs
@@ -14,36 +14,48 @@ namespace Infraestructure.Persistence.Repositories
 {
     public class ReceptionDocumentRepository : IReceptionDocumentRepository
     {
+        /// <summary>
+        /// Attributes.
+        /// </summary>
         protected readonly DbSet<ReceptionDocument> _receptions;
 
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="context"></param>
         public ReceptionDocumentRepository(ApplicationDbContext context)
         {
             _receptions = context.Set<ReceptionDocument>();
         }
 
-        public Task AddAsync(ReceptionDocument entity, CancellationToken ct = default)
+        /// <inheritdoc/>
+        public async Task AddAsync(ReceptionDocument entity, CancellationToken ct = default)
         {
-            throw new NotImplementedException();
+            await _receptions.AddAsync(entity, ct);
         }
 
-        public Task AddRangeAsync(IEnumerable<ReceptionDocument> entities, CancellationToken ct = default)
+        /// <inheritdoc/>
+        public async Task AddRangeAsync(IEnumerable<ReceptionDocument> entities, CancellationToken ct = default)
         {
-            throw new NotImplementedException();
+            await _receptions.AddRangeAsync(entities, ct);
         }
 
-        public Task<IEnumerable<ReceptionDocument>> FindAsync(Expression<Func<ReceptionDocument, bool>> predicate, CancellationToken ct = default)
+        /// <inheritdoc/>
+        public async Task<IEnumerable<ReceptionDocument>> FindAsync(Expression<Func<ReceptionDocument, bool>> predicate, CancellationToken ct = default)
         {
-            throw new NotImplementedException();
+            return await _receptions.Where(predicate).ToListAsync(ct);
         }
 
+        /// <inheritdoc/>
         public async Task<IEnumerable<ReceptionDocument>> GetAllAsync(CancellationToken ct = default)
         {
-            return await _receptions.ToListAsync();
+            return await _receptions.AsNoTracking().ToListAsync(ct);
         }
 
-        public Task<ReceptionDocument?> GetAsync(Guid id, CancellationToken ct = default)
+        /// <inheritdoc/>
+        public async Task<ReceptionDocument?> GetAsync(Guid id, CancellationToken ct = default)
         {
-            throw new NotImplementedException();
+            return await _receptions.FirstOrDefaultAsync(x => x.Id == id);
         }
     }
 }
