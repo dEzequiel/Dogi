c64dec1254157752adc709056434be1960724b96
Ezequiel De La Rosa <91556738+dEzequiel@users.noreply.github.com>
2023-04-01 22:01:25 +0200

    Testing AutoMoqData if working generationg parametrized object.

---
+++ Test/Domain/AnimalTest.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Entities;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test.Utils.Attributes;
+using Xunit;
+
+namespace Test.Domain
+{
+    public class AnimalTest
+    {
+        [Theory]
+        [AutoMoqData]
+        public void Should_Create_Animal(
+            ReceptionDocument receptionDocument)
+        {
+            // Arrange
+            var sut = Animal.Create(
+                Guid.NewGuid(),
+                receptionDocument.Id,
+                "Test",
+                1,
+                "Black");
+           
+            // Assert
+            Assert.True(sut.IsSuccess);
+            Assert.Equal(sut.Value.ReceptionDocumentId, receptionDocument.Id);
+        }
+    }
+}
