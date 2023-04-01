295e1b4b9da632ecaf26e091599d53513f8a63c6
Ezequiel De La Rosa <91556738+dEzequiel@users.noreply.github.com>
2023-04-01 20:54:34 +0200

    Add domain errors for Animal domain entity.

--- Domain/Exceptions/DomainErrors.cs
+++ Domain/Exceptions/DomainErrors.cs
@@ -22,6 +22,18 @@
             public static readonly Error ActorIsNullOrEmpty = new Error("ReceptionDocument.Actor", "Actor can't have null or default value.");
             public static readonly Error CategoryIsNullOrEmpty = new Error("ReceptionDocument.Category", "Category can't have null or default value.");
             public static readonly Error ColorIsEmpty = new Error("ReceptionDocument.Color", "Color can't be null or empty.");
-        }            
+        }
+
+        /// <summary>
+        /// Collection of Errors related to Animal Entity.
+        /// </summary>
+        public static class Animal
+        {
+            public static readonly Error AnimalIdIsNullOrEmpty = new Error("Animal.Id", "Animal Id can't have null or default value");
+            public static readonly Error ReceptionDocumentIdIsNullOrEmpty = new Error("Animal.ReceptionDocumentId", "ReceptionDocumentId can't have null or default value.");
+            public static readonly Error AnimalNameCantBeNullOrEmpty = new Error("Animal.Name", "Name can't have null or empty value.");
+            public static readonly Error AnimalAgeCantBeLowerThanZero = new Error("Animal.Age", "Age can't have value lower tan zero.");
+            public static readonly Error AnimalColorCantBeNullOrEmpty = new Error("Animal.Color", "Color can't have null or empty value.");
+        }
     }
 }
