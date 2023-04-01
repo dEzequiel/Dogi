9172383da40b1b6276aba965a2cb4c30dc6960f8
Ezequiel De La Rosa <91556738+dEzequiel@users.noreply.github.com>
2023-04-01 22:04:13 +0200

    Add explanation about configuration.

--- Test/Utils/Attributes/AutoMoqDataAttribute.cs
+++ Test/Utils/Attributes/AutoMoqDataAttribute.cs
@@ -4,6 +4,9 @@ using Test.Utils.Customizations;
 
 namespace Test.Utils.Attributes
 {
+    /// <summary>
+    /// Configuration with the purpose to generate parametrized data for testing.
+    /// </summary>
     public class AutoMoqDataAttribute : AutoDataAttribute
     {
         public AutoMoqDataAttribute()
