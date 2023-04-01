5e08e830adc0b5a2366299d0cde015f0868eb6a7
Ezequiel De La Rosa <91556738+dEzequiel@users.noreply.github.com>
2023-04-01 22:01:06 +0200

    Configure AutoFixture to work with Moq and XUnit2.

---
+++ Test/Utils/Attributes/AutoMoqDataAttribute.cs
@@ -0,0 +1,12 @@
+﻿using AutoFixture;
+using AutoFixture.Xunit2;
+using Test.Utils.Customizations;
+
+namespace Test.Utils.Attributes
+{
+    public class AutoMoqDataAttribute : AutoDataAttribute
+    {
+        public AutoMoqDataAttribute()
+            :base(() => new Fixture().Customize(new AutoFixtureCustomization())){ }
+    }
+}
