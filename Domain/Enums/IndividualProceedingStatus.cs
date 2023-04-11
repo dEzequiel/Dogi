438cf806659aa15babd2e7e5dfc9ba089a0e0819
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2023-04-11 20:02:32 +0200

    Implement enumerator for different status Individual Proceeding can have.

---
+++ Domain/Enums/IndividualProceedingStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Enums
+{
+    /// <summary>
+    /// Options that can have Individual Proceeding as status.
+    /// </summary>
+    public enum IndividualProceedingStatus
+    {
+        Close = 0,
+        Open = 1,
+        Adopted = 2
+    }
+}
