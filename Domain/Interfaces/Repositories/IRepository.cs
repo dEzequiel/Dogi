d38af4cd3af80c251f4e4083d4049c190dd1a6cc
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2023-04-14 22:52:53 +0200

    Remove CancellationToken. Not applying in this regular operations.

--- Domain/Interfaces/Repositories/IRepository.cs
+++ Domain/Interfaces/Repositories/IRepository.cs
@@ -19,14 +19,14 @@ namespace Domain.Interfaces.Repositories
         /// <param name="id"></param>
         /// <param name="ct"></param>
         /// <returns></returns>
-        Task<TEntity?> GetAsync(Guid id, CancellationToken ct = default);
+        Task<TEntity?> GetAsync(Guid id);
 
         /// <summary>
         ///  Retrieve all entities asynchronously.
         /// </summary>
         /// <param name="ct"></param>
         /// <returns></returns>
-        Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken ct = default);
+        Task<IEnumerable<TEntity>> GetAllAsync();
 
         /// <summary>
         /// Retrieve entity by identifier.
@@ -34,7 +34,7 @@ namespace Domain.Interfaces.Repositories
         /// <param name="predicate"></param>
         /// <param name="ct"></param>
         /// <returns></returns>
-        Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken ct = default);
+        Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate);
 
         /// <summary>
         /// Add entity asynchronously.
@@ -42,7 +42,7 @@ namespace Domain.Interfaces.Repositories
         /// <param name="entity"></param>
         /// <param name="ct"></param>
         /// <returns></returns>
-        Task AddAsync(TEntity entity, CancellationToken ct = default);
+        Task AddAsync(TEntity entity);
 
         /// <summary>
         /// Add collection of entities asynchronously.
@@ -50,7 +50,7 @@ namespace Domain.Interfaces.Repositories
         /// <param name="entities"></param>
         /// <param name="ct"></param>
         /// <returns></returns>
-        Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken ct = default);
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
     }
 
 }
