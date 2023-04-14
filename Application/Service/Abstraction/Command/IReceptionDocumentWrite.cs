5f1529f3949deace5edcc66e412c1ed865b55ca0
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2023-04-14 22:49:12 +0200

    Define ReceptionDocument service for write operations.

--- Application/Service/Abstraction/Command/IReceptionDocumentWrite.cs
+++ Application/Service/Abstraction/Command/IReceptionDocumentWrite.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Application.DTOs.ReceptionDocument;
+using Crosscuting.Base.Interfaces;
 
 namespace Application.Service.Abstraction.Command
 {
-    public interface IReceptionDocumentWrite
+    /// <summary>
+    /// ReceptionDocument write operations definition.
+    /// </summary>
+    public interface IReceptionDocumentWrite : IApplicationServiceBase
     {
+        /// <summary>
+        /// Add new ReceptionDocument.
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        Task<ReceptionDocumentForGet> AddAsync(ReceptionDocumentForAdd entity, CancellationToken ct = default);
+
+        /// <summary>
+        /// Update existing ReceptionDocument.
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        Task<ReceptionDocumentForGet?> UpdateAsync(ReceptionDocumentForUpdate entity, CancellationToken ct = default);
+
+        /// <summary>
+        /// Soft delete existing ReceptionDocument.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        Task<bool> LogicRemoveAsync(Guid id,  CancellationToken ct = default);
     }
 }
