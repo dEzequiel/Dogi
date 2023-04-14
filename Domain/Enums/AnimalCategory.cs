a9496cf9ce09bb36eb10dc8e71baabac685e9de3
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2023-04-14 15:51:02 +0200

    Implement provitional list of AnimalCategory.

--- Domain/Enums/AnimalCategory.cs
+++ Domain/Enums/AnimalCategory.cs
@@ -2,9 +2,17 @@
 namespace Domain.Enums
 {
     /// <summary>
-    /// Options representing the animal category to which the entity belongs.
+    /// Options representing the animal category to which the animal belongs.
     /// </summary>
     public enum AnimalCategory
     {
+        Dogs = 0,
+        Cats = 1,
+        Rabbits = 2,
+        Rodents = 3,
+        Birds = 4,
+        Reptiles = 5,
+        Exotic = 6,
+        Farm = 7
     }
 }
