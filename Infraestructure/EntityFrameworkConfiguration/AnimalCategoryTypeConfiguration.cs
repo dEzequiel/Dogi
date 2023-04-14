85b42c28e81e669ab7abd4ea135ffa90a08e586b
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2023-04-14 18:37:01 +0200

    Register EF Core Support tables configuration.

---
+++ Infraestructure/EntityFrameworkConfiguration/AnimalCategoryTypeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Infraestructure.Support;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infraestructure.EntityFrameworkConfiguration
+{
+    public class AnimalCategoryTypeConfiguration : IEntityTypeConfiguration<AnimalCategory>
+    {
+        public void Configure(EntityTypeBuilder<AnimalCategory> builder)
+        {
+            builder
+                .ToTable("AnimalCategory", "Dogi")
+                .HasKey(x => x.Id)
+                .IsClustered(false);
+
+            builder.HasData(
+                Enum.GetValues(typeof(Domain.Enums.AnimalCategory))
+                .Cast<Domain.Enums.AnimalCategory>()
+                .Select(e => new { Id = (int)e, Type = e.ToString() })
+            );
+        }
+    }
+}
