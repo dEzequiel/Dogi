2ca57f59ff6832bb6bbaa7891dd94ebc714cb33d
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2023-04-15 21:38:43 +0200

    Define class to throw Dogi exceptions.

---
+++ Crosscuting/Base/Exceptions/InvalidDataException.cs
@@ -0,0 +1,18 @@
+﻿namespace Crosscuting.Base.Exceptions
+{
+    public class InvalidDataException : Exception
+    {
+        /// <summary>
+        /// Exception with informational message.
+        /// </summary>
+        /// <param name="message"></param>
+        public InvalidDataException(string message) : base(message) { }
+
+        /// <summary>
+        /// Exception with an informational message and the inner exception raised.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="innerException"></param>
+        public InvalidDataException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
