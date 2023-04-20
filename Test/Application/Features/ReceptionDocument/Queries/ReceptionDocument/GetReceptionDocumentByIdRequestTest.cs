58c7caaa9a5adb5006d7dfe09844f5ea33bd0f98
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2023-04-20 19:50:34 +0200

    CQRS ReceptionDocument Feature to get Reception document by its identifier.

---
+++ Test/Application/Features/ReceptionDocument/Queries/ReceptionDocument/GetReceptionDocumentByIdRequestTest.cs
@@ -0,0 +1,42 @@
+﻿using Application.DTOs.ReceptionDocument;
+using Application.Features.ReceptionDocument.Queries;
+using Application.Service.Abstraction.Read;
+using AutoFixture.Xunit2;
+using Crosscuting.Api.DTOs.Response;
+using Moq;
+using Test.Utils.Attributes;
+
+namespace Test.Application.Features.ReceptionDocument.Queries.ReceptionDocument;
+
+public class GetReceptionDocumentByIdRequestTest
+{
+    [Theory]
+    [AutoMoqData]
+    internal void RequestShouldSetIdDataProperty(Guid id)
+    {
+        // Act
+        var request = new GetReceptionDocumentByIdRequest(id);
+        // Assert
+        Assert.Equal(expected: id, actual: request.Id);
+    }
+
+    [Theory]
+    [AutoMoqData]
+    internal async Task HandleShouldCallServiceAndReturnApiResponseDtoAsync(
+        [Frozen] Mock<IReceptionDocumentRead> receptionDocumentReadServiceMock,
+        ReceptionDocumentForGet documentForGet,
+        GetReceptionDocumentByIdRequest request,
+        GetReceptionDocumentByIdRequestHandler handler)
+    {
+        // Arrange
+        receptionDocumentReadServiceMock.Setup(x => x.GetByIdAsync(It.IsAny<Guid>()))
+            .ReturnsAsync(documentForGet);
+        
+        // Act
+        var result = await handler.Handle(request, default);
+
+        // Assert
+        Assert.IsType<ApiResponse<ReceptionDocumentForGet>>(result);
+        receptionDocumentReadServiceMock.Verify(x => x.GetByIdAsync(It.IsAny<Guid>()));
+    }
+}
