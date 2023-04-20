62311d0c1a36894229a1a81f4e0703e6090f20d4
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2023-04-20 15:45:17 +0200

    Implement classes for Pagination requests.

---
+++ Crosscuting/Api/DTOs/Response/PageResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace Crosscuting.Api.DTOs.Response;
+
+/// <inheritdoc/>
+public sealed class PageResponse<TData> : IPageResponse<TData> where TData : class
+{
+    /// <inheritdoc/>
+    public TData Data { get; set; }
+
+    /// <inheritdoc/>
+    public int TotalCount { get; set; }
+
+    public int NumPage { get; set; } = 1;
+
+    public int PageSize { get; set; } = 10;
+
+    public int TotalPages => PageSize > 0
+        ? (int)Math.Ceiling(TotalCount / (double)PageSize)
+        : 0;
+}
---
+++ Crosscuting/Api/DTOs/Response/PaginatedRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace Crosscuting.Api.DTOs.Response;
+
+/// <summary>
+/// Request to standardize entries to APIs
+/// </summary>
+public class PaginatedRequest
+{
+    /// <summary>
+    /// Number of the page
+    /// </summary>
+    public virtual int NumPage { get; set; }
+
+    /// <summary>
+    /// Size of the page
+    /// </summary>
+    public virtual int PageSize { get; set; }
+}
