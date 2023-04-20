24fb1d9b34270527f1a60b3ab5e1f40f8b65c36c
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2023-04-20 16:17:52 +0200

    Test: should throw invalid data exception if returned database data is not in valid state.

--- Test/Application/ReadServices/ReceptionDocumentReadServiceTest.cs
+++ Test/Application/ReadServices/ReceptionDocumentReadServiceTest.cs
@@ -6,9 +6,12 @@ using AutoFixture.Xunit2;
 using Crosscuting.Base.Exceptions;
 using Domain.Entities;
 using Domain.Exceptions;
+using Domain.Exceptions.Result;
 using Domain.Interfaces.Repositories;
 using Moq;
 using Test.Utils.Attributes;
+using InvalidDataException = Crosscuting.Base.Exceptions.InvalidDataException;
+
 
 namespace Test.Application.ReadServices;
 
@@ -43,14 +46,11 @@ public class ReceptionDocumentReadServiceTest
     internal async Task ShouldThrowDogiExceptionIfReceptionDocumentNotFoundAsync(
         [Frozen] Mock<IUnitOfWork> unitOfWorkMock,
         [Frozen] Mock<IReceptionDocumentRepository> repositoryMock,
-        ReceptionDocumentForGet documentGet,
         ReceptionDocument document,
         ReceptionDocumentRead sut)
     {
         // Arrange
         var EXCEPTION_RECEPTION_DOCUMENT_NOT_FOUND_MESSAGE = "RECEPTION DOCUMENT NOT FOUND";
-
-        documentGet.Id = document.Id;
         
         unitOfWorkMock.Setup(u => u.ReceptionDocumentRepository).Returns(repositoryMock.Object);
 
@@ -61,6 +61,33 @@ public class ReceptionDocumentReadServiceTest
         var ex = await Assert.ThrowsAsync<DogiException>(() => sut.GetByIdAsync(document.Id));
         Assert.Equal(EXCEPTION_RECEPTION_DOCUMENT_NOT_FOUND_MESSAGE, ex.Message);
     }
-    
-    
+
+    [Theory]
+    [AutoMoqData]
+    internal async Task ShouldThrowInvalidDataExceptionIfReturnedDocumentNotValidAsync(
+        [Frozen] Mock<IUnitOfWork> unitOfWorkMock,
+        [Frozen] Mock<IReceptionDocumentRepository> repositoryMock,
+        [Frozen] Mock<Domain.Entities.ReceptionDocument> receptionDocumentEntityMock,
+        ReceptionDocument document,
+        ReceptionDocumentForAdd documentAdd,
+        ReceptionDocumentRead sut)
+    {
+        // Arrange
+        var invalidDocument = new Domain.Entities.ReceptionDocument(Guid.Empty, documentAdd.HasChip, 
+            documentAdd.Observations, 
+            documentAdd.PickupLocation, 
+            documentAdd.PickupDate);
+        
+        unitOfWorkMock.Setup(u => u.ReceptionDocumentRepository).Returns(repositoryMock.Object);
+
+        repositoryMock.Setup(r => r.GetAsync(It.IsAny<Guid>()))
+            .ReturnsAsync(invalidDocument);
+        
+        receptionDocumentEntityMock.Setup(x => x.Verify(invalidDocument))
+            .Returns(Result.Failure<Domain.Entities.ReceptionDocument>(DomainErrors.ReceptionDocument.ReceptionIdIsNullOrEmpty));
+        
+        // Act & Assert
+        var ex = await Assert.ThrowsAsync<InvalidDataException>(() => sut.GetByIdAsync(document.Id));
+        Assert.Equal(DomainErrors.ReceptionDocument.ReceptionIdIsNullOrEmpty.Message, ex.Message);
+    }
 }
