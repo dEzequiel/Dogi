10380ee5436a1b9e1d0902032188fdde2d975e54
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2023-04-20 16:08:15 +0200

    Test: should throw DogiException if ReceptionDocument not found by id.

--- Test/Application/ReadServices/ReceptionDocumentReadServiceTest.cs
+++ Test/Application/ReadServices/ReceptionDocumentReadServiceTest.cs
@@ -3,7 +3,9 @@ using Application.Service.Implementation.Command;
 using Application.Service.Implementation.Read;
 using Application.Service.Interfaces;
 using AutoFixture.Xunit2;
+using Crosscuting.Base.Exceptions;
 using Domain.Entities;
+using Domain.Exceptions;
 using Domain.Interfaces.Repositories;
 using Moq;
 using Test.Utils.Attributes;
@@ -35,4 +37,30 @@ public class ReceptionDocumentReadServiceTest
         // Assert
         Assert.Equal(document.Id, documentGet.Id);
     }
+
+    [Theory]
+    [AutoMoqData]
+    internal async Task ShouldThrowDogiExceptionIfReceptionDocumentNotFoundAsync(
+        [Frozen] Mock<IUnitOfWork> unitOfWorkMock,
+        [Frozen] Mock<IReceptionDocumentRepository> repositoryMock,
+        ReceptionDocumentForGet documentGet,
+        ReceptionDocument document,
+        ReceptionDocumentRead sut)
+    {
+        // Arrange
+        var EXCEPTION_RECEPTION_DOCUMENT_NOT_FOUND_MESSAGE = "RECEPTION DOCUMENT NOT FOUND";
+
+        documentGet.Id = document.Id;
+        
+        unitOfWorkMock.Setup(u => u.ReceptionDocumentRepository).Returns(repositoryMock.Object);
+
+        repositoryMock.Setup(r => r.GetAsync(It.IsAny<Guid>()))
+            .ReturnsAsync((ReceptionDocument?)null);
+        
+        // Act & Assert
+        var ex = await Assert.ThrowsAsync<DogiException>(() => sut.GetByIdAsync(document.Id));
+        Assert.Equal(EXCEPTION_RECEPTION_DOCUMENT_NOT_FOUND_MESSAGE, ex.Message);
+    }
+    
+    
 }
