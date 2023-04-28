5b5973601498500b8192b51ac5c34a49a23efe21
Ezequiel De La Rosa <91556738+dEzequiel@users.noreply.github.com>
2023-04-29 00:43:14 +0200

    Reorganize object types and object input types.

---
+++ Api/GraphQL/InputObjectTypes/ReceptionDocumentInput.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+
+namespace Api.GraphQL.InputObjectTypes
+{
+    public class ReceptionDocumentInputType : InputObjectType<ReceptionDocument>
+    {
+        protected override void Configure(IInputObjectTypeDescriptor<ReceptionDocument> descriptor)
+        {
+            descriptor.Field(f => f.Id)
+              .Type<UuidType>()
+              .DefaultValue(Guid.NewGuid());
+
+            descriptor.Field(f => f.HasChip)
+                .Type<BooleanType>();
+
+            descriptor.Field(f => f.Observations)
+                .Type<StringType>();
+
+            descriptor.Field(f => f.PickupLocation)
+                .Type<StringType>();
+
+            descriptor.Field(f => f.PickupDate)
+                .Type<DateTimeType>();
+        }
+    }
+
+}
---
+++ Api/GraphQL/ObjectTypes/ReceptionDocumentType.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities;
+
+namespace Api.GraphQL.GraphQLTypes;
+
+/// <summary>
+/// ReceptionDocument HotChocolate Type implementation.
+/// </summary>
+public class ReceptionDocumentType : ObjectType<ReceptionDocument>
+{
+    protected override void Configure(IObjectTypeDescriptor<ReceptionDocument> descriptor)
+    {
+        descriptor
+            .Field(f => f.Id)
+            .Type<NonNullType<UuidType>>();
+
+        descriptor.Field(f => f.HasChip)
+            .Type<BooleanType>();
+
+        descriptor.Field(f => f.Observations)
+            .Type<StringType>();
+
+        descriptor.Field(f => f.PickupLocation)
+            .Type<StringType>();
+
+        descriptor.Field(f => f.PickupDate)
+            .Type<DateType>();
+
+        descriptor.Ignore(f => f.IndividualProceeding);
+    }
+}
