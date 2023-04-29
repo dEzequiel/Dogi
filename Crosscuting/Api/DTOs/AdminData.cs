80b3068d70a31eb0fb802ef2ed53d45d25add71d
Ezequiel De La Rosa <91556738+dEzequiel@users.noreply.github.com>
2023-04-29 17:29:34 +0200

    Class to represent admin.

---
+++ Crosscuting/Api/DTOs/AdminData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crosscuting.Api.DTOs
+{
+    /// <summary>
+    /// Represents the animal shelter worker.
+    /// </summary>
+    public class AdminData
+    {
+        /// <summary>
+        /// Attributes.
+        /// </summary>
+        public Guid Id { get; set; }
+        public string Email { get; set; } = null!;
+    }
+}
