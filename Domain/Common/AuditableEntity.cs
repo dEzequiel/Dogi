2e2a2028036029e68c53a26799ed6b06e5beb81d
Ezequiel De La Rosa <91556738+dEzequiel@users.noreply.github.com>
2023-04-29 17:25:31 +0200

    Refactor ReceptionDocument as auditable entity.

--- Domain/Common/AuditableEntity.cs
+++ Domain/Common/AuditableEntity.cs
@@ -2,16 +2,33 @@
 {
     public abstract class AuditableEntity : Entity
     {
-        protected AuditableEntity(Guid id) : base(id)
-        {
-        }
-
+        /// <summary>
+        /// Creation date.
+        /// </summary>
         public DateTime Created { get; set; }
 
+        /// <summary>
+        /// Creation actor.
+        /// </summary>
         public string? CreatedBy { get; set; }
 
+        /// <summary>
+        /// Last modification date.
+        /// </summary>
         public DateTime? LastModified { get; set; }
 
+        /// <summary>
+        /// Last modifier actor.
+        /// </summary>
         public string? LastModifiedBy { get; set; }
+
+        /// <summary>
+        /// Entity constructor that initializes its identifier
+        /// </summary>
+        /// <param name="id">Entity identifier</param>
+        protected AuditableEntity(Guid id) : base(id)
+        {
+            Id = id;
+        }
     }
 }
