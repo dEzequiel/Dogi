f1924d1100c86fbfc8aa3b1f834538994332dac4
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2023-05-08 19:25:27 +0200

    Interface for IndividualProceeding write operations.

---
+++ Application/Service/Abstraction/Write/IIndividualProceeding.cs
@@ -0,0 +1,29 @@
+﻿using Crosscuting.Api.DTOs;
+using Domain.Entities;
+
+namespace Application.Service.Abstraction.Write
+{
+    /// <summary>
+    /// IndividualProceeding Write Service Definition.
+    /// </summary>
+    public interface IIndividualProceeding
+    {
+        /// <summary>
+        /// Add new IndividualProceeding.
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="admin"></param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        Task<IndividualProceeding> AddAsync(IndividualProceeding entity, AdminData admin, CancellationToken ct = default);
+
+        /// <summary>
+        /// Soft delete existing IndividualProceeding.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="admin"></param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        Task<bool> LogicRemoveAsync(Guid id, AdminData admin, CancellationToken ct = default);
+    }
+}
