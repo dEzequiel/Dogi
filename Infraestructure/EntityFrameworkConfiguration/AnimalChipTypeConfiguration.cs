883bfbffa92164ee955ff3a8cfb8357f5efb1e8c
Ezequiel De La Rosa <91556738+dEzequiel@users.noreply.github.com>
2023-05-11 19:23:51 +0200

    Reconfigure AnimalChip table.

--- Infraestructure/EntityFrameworkConfiguration/AnimalChipTypeConfiguration.cs
+++ Infraestructure/EntityFrameworkConfiguration/AnimalChipTypeConfiguration.cs
@@ -14,9 +14,18 @@ namespace Infraestructure.EntityFrameworkConfiguration
                 .HasKey(x => x.Id)
                 .IsClustered(false);
 
-            builder.HasOne<AnimalChipOwner>(f => f.AnimalChipOwner)
-                .WithOne(r => r.AnimalChip)
-                .HasForeignKey<AnimalChip>(r => r.AnimalChipOwnerId);
+            builder.OwnsOne(vo => vo.Owner, owner =>
+            {
+                owner.Property(x => x.Name).HasColumnName("OwnerName");
+                owner.Property(x => x.Lastname).HasColumnName("OwnerLastName");
+                owner.Property(x => x.Contact).HasColumnName("OwnerContact");
+                owner.OwnsOne(z => z.Address, address =>
+                {
+                    address.Property(x => x.Street).HasColumnName("OwnerAddressStreet");
+                    address.Property(x => x.City).HasColumnName("OwnerAddressCity");
+                    address.Property(x => x.ZipCode).HasColumnName("OwnerAddressZipCode");
+                });
+            });
 
 
         }
