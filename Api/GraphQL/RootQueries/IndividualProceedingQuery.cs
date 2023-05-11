2caa1c2ff2cc018af4f13d595012a3e2c3c67cb8
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2023-05-11 09:15:32 +0200

    IndividualProceeding queries.

---
+++ Api/GraphQL/RootQueries/IndividualProceedingQuery.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+
+namespace Api.GraphQL.GraphQLQueries
+{
+    /// <summary>
+    /// Public queries.
+    /// </summary>
+    public partial class Query
+    {
+        /// <summary>
+        /// Animal entity public queries.
+        /// </summary>
+        public IndividualProceeding? IndividualProceeding { get; }
+    }
+}
