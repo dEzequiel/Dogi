08b334ccb57d4ff467f02e79ecd0162899b99414
Ezequiel De La Rosa <e.delarosa@innovation.es>
2023-05-15 11:06:49 +0200

    Guard no nullable properties.

--- Application/Service/Implementation/Write/IndividualProceedingWrite.cs
+++ Application/Service/Implementation/Write/IndividualProceedingWrite.cs
@@ -27,6 +27,8 @@ namespace Application.Service.Implementation.Write
             Guard.Against.Null(entity, nameof(entity));
             Guard.Against.NullOrEmpty(entity.ReceptionDocumentId, nameof(entity.ReceptionDocumentId));
             Guard.Against.Null(entity.StatusId, nameof(entity.StatusId));
+            Guard.Against.Null(entity.SexId, nameof(entity.SexId));
+            Guard.Against.Null(entity.ZoneId, nameof(entity.ZoneId));
             Guard.Against.Null(admin, nameof(admin));
             Guard.Against.NullOrEmpty(admin.Id, nameof(admin.Id));
             Guard.Against.NullOrEmpty(admin.Email, nameof(admin.Email));
