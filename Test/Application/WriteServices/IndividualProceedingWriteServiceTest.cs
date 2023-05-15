96d2a6c1f51b7f9c589810bd882f966451a098a2
Ezequiel De La Rosa <e.delarosa@innovation.es>
2023-05-15 11:02:10 +0200

    IndividualProceedingWriteService unit tests.

--- Test/Application/WriteServices/IndividualProceedingWriteServiceTest.cs
+++ Test/Application/WriteServices/IndividualProceedingWriteServiceTest.cs
@@ -5,11 +5,6 @@ using AutoFixture.Xunit2;
 using Crosscuting.Api.DTOs;
 using Domain.Entities;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Test.Utils.Attributes;
 
 namespace Test.Application.WriteServices
@@ -26,21 +21,77 @@ namespace Test.Application.WriteServices
             IndividualProceedingWrite sut)
         {
             // Arrange
-            CancellationToken cancellationToken = new CancellationToken();
-            individualProceedingAdd.ReceptionDocumentId = individualProceedingAdd.ReceptionDocument.Id;
-            //individualProceedingAdd.StatusId = individualProceedingAdd.Status.Id;
-
-            unitOfWorkMock.Setup(u => u.IndividualProceedingRepository).Returns(repositoryMock.Object);
-
-            repositoryMock.Setup(r => r.AddAsync(It.IsAny<IndividualProceeding>(), It.IsAny<AdminData>(), It.IsAny<CancellationToken>()))
+            unitOfWorkMock.Setup(u => u.IndividualProceedingRepository)
+                .Returns(repositoryMock.Object);
+            
+            repositoryMock.Setup(r => r.AddAsync(
+                It.IsAny<IndividualProceeding>(),
+                It.IsAny<AdminData>(),
+                It.IsAny<CancellationToken>()))
                 .Returns(Task.CompletedTask);
-
-            // Act
-            var result = await sut.AddAsync(individualProceedingAdd, admin, cancellationToken);
+            
+            // act
+            var result = await sut.AddAsync(individualProceedingAdd, admin);
 
             // Assert
-            Assert.Equal(result.StatusId, individualProceedingAdd.StatusId);
-            Assert.Equal(result.ReceptionDocumentId, individualProceedingAdd.ReceptionDocumentId);
+            Assert.NotNull(result);
+            Assert.IsType<IndividualProceeding>(result);
+
+            unitOfWorkMock.Verify(u => u.IndividualProceedingRepository, Times.Once);
+            repositoryMock.Verify(r => r.AddAsync(
+                It.IsAny<IndividualProceeding>(),
+                It.IsAny<AdminData>(),
+                It.IsAny<CancellationToken>()), Times.Once);
+            unitOfWorkMock.Verify(u => u.CompleteAsync(It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        internal async Task ShouldntAddNewIndividualProceedingIfIsNullAsync(
+            [Frozen] Mock<IUnitOfWork> unitOfWorkMock,
+            [Frozen] Mock<IIndividualProceedingRepository> repositoryMock,
+            [Frozen] AdminData admin,
+            IndividualProceedingWrite sut)
+        {
+            // Arrange
+            IndividualProceeding individualProceedingAdd = null!;
+
+            unitOfWorkMock.Setup(u => u.IndividualProceedingRepository)
+                .Returns(repositoryMock.Object);
+
+            repositoryMock.Setup(r => r.AddAsync(
+                It.IsAny<IndividualProceeding>(),
+                It.IsAny<AdminData>(),
+                It.IsAny<CancellationToken>()))
+                .Returns(Task.CompletedTask);
+
+            // act & assert
+            await Assert.ThrowsAsync<ArgumentNullException>(() => sut.AddAsync(individualProceedingAdd, admin));
+        }
+
+        [Theory]
+        [AutoMoqData]
+        internal async Task ShouldnAddNewIndividualProceedingIfAdminIsNullAsync(
+            [Frozen] Mock<IUnitOfWork> unitOfWorkMock,
+            [Frozen] Mock<IIndividualProceedingRepository> repositoryMock,
+            [Frozen] IndividualProceeding individualProceedingAdd,
+            IndividualProceedingWrite sut)
+        {
+            // Arrange
+            AdminData admin = null!;
+
+            unitOfWorkMock.Setup(u => u.IndividualProceedingRepository)
+                .Returns(repositoryMock.Object);
+
+            repositoryMock.Setup(r => r.AddAsync(
+                It.IsAny<IndividualProceeding>(),
+                It.IsAny<AdminData>(),
+                It.IsAny<CancellationToken>()))
+                .Returns(Task.CompletedTask);
+
+            // act & assert
+            await Assert.ThrowsAsync<ArgumentNullException>(() => sut.AddAsync(individualProceedingAdd, admin));
         }
     }
 }
+
