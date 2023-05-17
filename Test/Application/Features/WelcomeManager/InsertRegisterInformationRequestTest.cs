a4612d2becf221d1d6ed11105fe4c67b48633acc
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2023-05-17 20:19:07 +0200

    Test CQRS Feature insert register information. Implementation.

--- Test/Application/Features/WelcomeManager/InsertRegisterInformationRequestTest.cs
+++ Test/Application/Features/WelcomeManager/InsertRegisterInformationRequestTest.cs
@@ -1,50 +1,55 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Application.DTOs.WelcomeManager;
 using Application.Features.WelcomeManagerFeature.Command;
 using Application.Managers;
 using AutoFixture.Xunit2;
 using Crosscuting.Api.DTOs;
 using Crosscuting.Api.DTOs.Response;
+using MediatR;
 using Moq;
 using Test.Utils.Attributes;
 
 namespace Test.Application.Features.WelcomeManagerTest
 {
-    public class InsertReceptionDocumentWithAnimalOwnerInfoRequestTest
+    public class InsertRegisterInformationRequestTest
     {
         [Theory]
         [AutoMoqData]
-        internal void RequestShouldSetReceptionDocumentDataProperty(RegisterInformation documentDataForAdd,
+        internal void RequestShouldSetRegisterInformationDataProperty(RegisterInformation documentDataForAdd,
             AdminData adminData)
         {
             // Act
-            var request = new InsertReceptionDocumentWithAnimalOwnerInfoRequest(documentDataForAdd, adminData);
+            var request = new InsertRegisterInformationRequest(documentDataForAdd, adminData);
             // Assert
             Assert.Equal(expected: documentDataForAdd, actual: request.Data);
         }
 
         [Theory]
         [AutoMoqData]
-        internal async Task HandleShouldManagerAndReturnApiResponseDtoAsync([Frozen] Mock<IWelcomeManager> welcomeManagerWriteServiceMock,
+        internal async Task HandleShouldCallManagerServiceWhenAnimalHasNoChipAndReturnApiResponseDtoAsync(
+            [Frozen] Mock<IWelcomeManager> welcomeManagerWriteServiceMock,
+            [Frozen] Mock<IMediator> mediator,
             RegisterInformation registerInformationGet,
             AdminData adminData,
-            InsertReceptionDocumentWithAnimalOwnerInfoRequest request,
-            InsertReceptionDocumentWithAnimalOwnerInfoRequestHandler handler)
+            InsertRegisterInformationRequest request,
+            InsertRegisterInformationRequestHandler handler)
         {
             // Arrange
+            request.Data.ReceptionDocument.HasChip = false;
+
             welcomeManagerWriteServiceMock.Setup(x => x.RegisterAnimal(
                 It.IsAny<RegisterInformation>(),
                 It.IsAny<AdminData>()))
                 .ReturnsAsync(registerInformationGet);
-            
+
             // Act
             var result = await handler.Handle(request, default);
 
             // Assert
             Assert.IsType<ApiResponse<RegisterInformation>>(result);
+            welcomeManagerWriteServiceMock.Verify(i => i.RegisterAnimal(
+                               It.IsAny<RegisterInformation>(),
+                               It.IsAny<AdminData>()), Times.Once());
+
         }
     }
 
