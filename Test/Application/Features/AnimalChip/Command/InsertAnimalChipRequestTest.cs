b130d6397d2bb6e881d7e1f82cdaadb44000e3ac
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2023-05-17 19:02:14 +0200

    Test CQRS Feature Insert AnimalChip.

--- Test/Application/Features/AnimalChip/Command/InsertAnimalChipRequestTest.cs
+++ Test/Application/Features/AnimalChip/Command/InsertAnimalChipRequestTest.cs
@@ -1,5 +1,9 @@
 ﻿using Application.Features.InsertAnimalChipRequest.Commands;
+using Application.Service.Abstraction.Write;
+using AutoFixture.Xunit2;
 using Crosscuting.Api.DTOs;
+using Crosscuting.Api.DTOs.Response;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +24,30 @@ namespace Test.Application.Features.AnimalChip.Command
             var request = new InsertAnimalChipRequest(data, adminData);
             // Assert
             Assert.Equal(expected: data, actual: request.AnimalChipData);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        internal async Task HandleShouldCallServiceAndReturnApiResponseDtoAsync(
+            [Frozen] Mock<IAnimalChipWrite> animalChipWriteMock,
+            Domain.Entities.AnimalChip animalChipForGet,
+            InsertAnimalChipRequest request,
+            InsertAnimalChipRequestHandler handler)
+        {
+            // Arrange
+            animalChipWriteMock.Setup(x => x.AddAsync(It.IsAny<Domain.Entities.AnimalChip>(),
+                                                      It.IsAny<AdminData>(),
+                                                      It.IsAny<CancellationToken>()))
+                .ReturnsAsync(animalChipForGet);
+            
+            // Act
+            var result = await handler.Handle(request, default);
+
+            // Assert
+            Assert.IsType<ApiResponse<Domain.Entities.AnimalChip>>(result);
+            animalChipWriteMock.Verify(i => i.AddAsync(It.IsAny<Domain.Entities.AnimalChip>(),
+                                                       It.IsAny<AdminData>(),
+                                                       It.IsAny<CancellationToken>()), Times.Once);
         }
     }
 }
