9e6654b42848a4761e1ccf7c8477219a67b40066
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2023-05-17 20:36:21 +0200

    Test WelcomeManager implementation.

--- Test/Application/Managers/WelcomeManagerTest.cs
+++ Test/Application/Managers/WelcomeManagerTest.cs
@@ -1,13 +1,13 @@
 using Application.DTOs.WelcomeManager;
-using Application.Interfaces.Repositories;
+using Application.Features.IndividualPro.Commands;
+using Application.Features.InsertAnimalChipRequest.Commands;
+using Application.Features.ReceptionDocument.Commands;
 using Application.Managers;
-using Application.Service.Abstraction;
-using Application.Service.Abstraction.Write;
-using Application.Service.Interfaces;
 using AutoFixture.Xunit2;
 using Crosscuting.Api.DTOs;
+using Crosscuting.Api.DTOs.Response;
 using Domain.Entities;
-using Domain.Enums;
+using MediatR;
 using Moq;
 using Test.Utils.Attributes;
 
@@ -17,58 +17,80 @@ namespace Test.Application.Managers
     {
         [Theory]
         [AutoMoqData]
-        internal async Task ShouldAddReceptionDocumentWithAnimalChipOwnerInformationIfDontHasChipAsync(
-            ReceptionDocumentWithAnimalInformation receptionDocumentWithAnimalChipOwnerInformation,
-            ReceptionDocument receptionDocumentGet,
-            IndividualProceeding individualProceedingGet,
+        internal async Task ShouldRegisterInformationAboutReceptionDocumentAndIndividualProceedingIfAnimalHasNoChipAsync(
+            [Frozen] Mock<IMediator> mediatorMock,
+            ApiResponse<ReceptionDocument> receptionDocumentApiResponse,
+            ApiResponse<IndividualProceeding> individualProceedingApiResponse,
+            RegisterInformation registerInformation,
             AdminData adminData,
-            [Frozen] Mock<IReceptionDocumentWrite> recepDocWriteMock,
-            [Frozen] Mock<IIndividualProceedingWrite> individualProceedingWriteMock,
-            [Frozen] Mock<IAnimalZoneRepository> animalZoneRepositoryMock,
-            [Frozen] Mock<IUnitOfWork> unitOfWorkMock,
-            WelcomeManager sut) {
-         {
-
+            WelcomeManager sut)
+        {
             // Arrange
-            individualProceedingGet.ReceptionDocumentId = receptionDocumentGet.Id;
-            individualProceedingGet.ZoneId = (int)AnimalZone.Quarantine;
-            receptionDocumentWithAnimalChipOwnerInformation.ReceptionDocument.HasChip = false;
+            registerInformation.ReceptionDocument.HasChip = false;
 
-            unitOfWorkMock.Setup(u => u.AnimalZoneRepository)
-                .Returns(animalZoneRepositoryMock.Object);
+            mediatorMock.Setup(x => x.Send(It.IsAny<InsertReceptionDocumentRequest>(),
+                                           It.IsAny<CancellationToken>()))
+                .ReturnsAsync(receptionDocumentApiResponse);
 
-            recepDocWriteMock.Setup(r => r.AddAsync(
-                It.IsAny<ReceptionDocument>(),
-                It.IsAny<AdminData>(),
-                It.IsAny<CancellationToken>()))
-                .ReturnsAsync(receptionDocumentGet);
-            
-            individualProceedingWriteMock.Setup(i => i.AddAsync(
-                It.IsAny<IndividualProceeding>(),
-                It.IsAny<AdminData>(),
-                It.IsAny<CancellationToken>()))
-                .ReturnsAsync(individualProceedingGet);
+            mediatorMock.Setup(x => x.Send(It.IsAny<InsertIndividualProceedingRequest>(),
+                               It.IsAny<CancellationToken>()))
+                .ReturnsAsync(individualProceedingApiResponse);
 
             // Act
-            var result = await sut.AddAnimalWithOwnerInfo(receptionDocumentWithAnimalChipOwnerInformation, adminData);
+            var result = await sut.RegisterAnimal(registerInformation, adminData);
 
             // Assert
             Assert.NotNull(result);
             Assert.IsType<RegisterInformation>(result);
+            mediatorMock.Verify(x => x.Send(It.IsAny<InsertReceptionDocumentRequest>(),
+                                            It.IsAny<CancellationToken>()), Times.Once);
 
-            Assert.Equal(receptionDocumentGet.Id, result.IndividualProceeding!.ReceptionDocumentId);
-            Assert.Equal(individualProceedingGet.Id, result.IndividualProceeding!.Id);
-            Assert.Equal(((int)AnimalZone.Quarantine), result.IndividualProceeding!.ZoneId);
+            mediatorMock.Verify(x => x.Send(It.IsAny<InsertIndividualProceedingRequest>(),
+                                            It.IsAny<CancellationToken>()), Times.Once);
 
-            recepDocWriteMock.Verify(r => r.AddAsync(
-                It.IsAny<ReceptionDocument>(),
-                It.IsAny<AdminData>(),
-                It.IsAny<CancellationToken>()), Times.Once);
-            
-            individualProceedingWriteMock.Verify(i => i.AddAsync(
-                It.IsAny<IndividualProceeding>(),
-                It.IsAny<AdminData>(),
-                It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        internal async Task ShouldRegisterInformationAboutReceptionDocumentIndividualProceedingAndAnimalChipIfAnimalHasChipAsync(
+            [Frozen] Mock<IMediator> mediatorMock,
+            ApiResponse<ReceptionDocument> receptionDocumentApiResponse,
+            ApiResponse<IndividualProceeding> individualProceedingApiResponse,
+            ApiResponse<AnimalChip> animalChipApiResponse,
+            RegisterInformation registerInformation,
+            AdminData adminData,
+            WelcomeManager sut)
+        {
+            // Arrange
+            registerInformation.ReceptionDocument.HasChip = true;
+
+            mediatorMock.Setup(x => x.Send(It.IsAny<InsertReceptionDocumentRequest>(),
+                                           It.IsAny<CancellationToken>()))
+                .ReturnsAsync(receptionDocumentApiResponse);
+
+            mediatorMock.Setup(x => x.Send(It.IsAny<InsertIndividualProceedingRequest>(),
+                               It.IsAny<CancellationToken>()))
+                .ReturnsAsync(individualProceedingApiResponse);
+
+            mediatorMock.Setup(x => x.Send(It.IsAny<InsertAnimalChipRequest>(),
+                               It.IsAny<CancellationToken>()))
+                .ReturnsAsync(animalChipApiResponse);
+
+            // Act
+            var result = await sut.RegisterAnimal(registerInformation, adminData);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<RegisterInformation>(result);
+            mediatorMock.Verify(x => x.Send(It.IsAny<InsertReceptionDocumentRequest>(),
+                                            It.IsAny<CancellationToken>()), Times.Once);
+
+            mediatorMock.Verify(x => x.Send(It.IsAny<InsertIndividualProceedingRequest>(),
+                                            It.IsAny<CancellationToken>()), Times.Once);
+
+            mediatorMock.Verify(x => x.Send(It.IsAny<InsertAnimalChipRequest>(),
+                                It.IsAny<CancellationToken>()), Times.Once);
+
         }
     }
-}}
+}
