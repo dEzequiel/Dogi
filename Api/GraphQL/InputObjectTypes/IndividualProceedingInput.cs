16e847c8e701f5d9099ba6950383670502229353
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2023-05-20 12:28:59 +0200

    Fix hotchocolate input and types. Add autogenerate configuration.

--- Api/GraphQL/InputObjectTypes/IndividualProceedingInput.cs
+++ Api/GraphQL/InputObjectTypes/IndividualProceedingInput.cs
@@ -9,17 +9,10 @@ namespace Api.GraphQL.InputObjectTypes
     {
         protected override void Configure(IInputObjectTypeDescriptor<IndividualProceeding> descriptor)
         {
+            descriptor.Field(f => f.Name).Type<NonNullType<StringType>>();
 
-            descriptor.Field(f => f.Name)
-                .Type<StringType>();
-
-            descriptor.Field(f => f.Age)
-                .Type<IntType>();
-
-            descriptor.Field(f => f.Color)
-                .Type<StringType>();
-
-            descriptor.Ignore(f => f.StatusId);
+            descriptor.Field(f => f.Age).Type<IntType>();
+            descriptor.Field(f => f.Color).Type<StringType>();
 
             descriptor.Field(f => f.CategoryId)
                 .Type<NonNullType<IntType>>();
@@ -27,17 +20,18 @@ namespace Api.GraphQL.InputObjectTypes
             descriptor.Field(f => f.SexId)
                 .Type<NonNullType<IntType>>();
 
-
             descriptor.Ignore(f => f.Id);
             descriptor.Ignore(f => f.ReceptionDocumentId);
             descriptor.Ignore(f => f.CageId);
-            descriptor.Ignore(f => f.Cage);
-            descriptor.Ignore(f => f.IsDeleted);
             descriptor.Ignore(f => f.StatusId);
+            descriptor.Ignore(f => f.IsDeleted);
+
             descriptor.Ignore(f => f.ReceptionDocument);
             descriptor.Ignore(f => f.ProceedingStatus);
             descriptor.Ignore(f => f.AnimalCategory);
             descriptor.Ignore(f => f.Sex);
+            descriptor.Ignore(f => f.Cage);
+
             descriptor.Ignore(f => f.Created);
             descriptor.Ignore(f => f.CreatedBy);
             descriptor.Ignore(f => f.LastModified);
--- Domain/Entities/IndividualProceeding.cs
+++ Domain/Entities/IndividualProceeding.cs
@@ -3,29 +3,69 @@ using Domain.Support;
 
 namespace Domain.Entities
 {
+    /// <summary>
+    /// Represents an animal in the shelter.
+    /// </summary>
     public class IndividualProceeding : AuditableEntity
     {
         /// <summary>
-        /// Attributes.
+        /// Nested reception document when animal first arrives.  
         /// </summary>
         public Guid ReceptionDocumentId { get; set; }
+        /// <summary>
+        /// Animal name.
+        /// </summary>
         public string? Name { get; set; }
+        /// <summary>
+        /// Animal age.
+        /// </summary>
         public int? Age { get; set; }
+        /// <summary>
+        /// Animal color.
+        /// </summary>
         public string? Color { get; set; }
+        /// <summary>
+        /// Animal status.
+        /// </summary>
         public int StatusId { get; set; }
         //public Guid MedicalRecordId { get; private set; }
+        /// <summary>
+        /// AnIMAL category.
+        /// </summary>
         public int CategoryId { get; set; }
+        /// <summary>
+        /// Animal sex.
+        /// </summary>
         public int SexId { get; set; }
+        /// <summary>
+        /// Animal cage.
+        /// </summary>
         public Guid CageId { get; set; }
         public bool IsDeleted { get; set; } = false;
 
         /// <summary>
         /// Navigation properties.
         /// </summary>
+
+        /// <summary>
+        /// Animal reception document.
+        /// </summary>
         public virtual ReceptionDocument ReceptionDocument { get; set; } = null!;
+        /// <summary>
+        /// Animal status.
+        /// </summary>
         public virtual ProceedingStatus ProceedingStatus { get; set; } = null!;
+        /// <summary>
+        /// Animal category.
+        /// </summary>
         public virtual AnimalCategory AnimalCategory { get; set; } = null!;
+        /// <summary>
+        /// Animal sex.
+        /// </summary>
         public virtual Sex Sex { get; set; } = null!;
+        /// <summary>
+        /// Animal cage.
+        /// </summary>
         public virtual Cage Cage { get; set; } = null!;
 
         public IndividualProceeding(Guid id) : base(id) { }
