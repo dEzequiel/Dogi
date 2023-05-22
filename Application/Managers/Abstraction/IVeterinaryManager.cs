70e5bdc29539e2dffd440bc592764844cb644bd7
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2023-05-22 22:00:17 +0200

    Define veterinary manager

--- Application/Managers/Abstraction/IVeterinaryManager.cs
+++ Application/Managers/Abstraction/IVeterinaryManager.cs
@@ -1,6 +1,15 @@
-﻿namespace Application.Managers.Abstraction
+﻿using Crosscuting.Base.Interfaces;
+using Domain.Entities;
+
+namespace Application.Managers.Abstraction
 {
-    internal interface Interface1
+    public interface IVeterinaryManager : IApplicationServiceBase
     {
+        /// <summary>
+        /// Move an animal that is in the quarantine area to the waiting area for medical examination. 
+        /// The status of the medical record is waiting. It is the first phase to enter a normal zone.
+        /// </summary>
+        /// <returns></returns>
+        Task SetForMedicalRevision(IndividualProceeding individualProceeding);
     }
 }
