86c2b0e13e158f0cfb469570b797b3bb0a5ad098
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2023-05-23 21:39:54 +0200

    Request and handler implementation for checking medical record.

---
+++ Application/DTOs/VeterinaryManager/InvidiualProceedingWithMedicalRecord.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+
+namespace Application.DTOs.VeterinaryManager
+{
+    public class InvidiualProceedingWithMedicalRecord
+    {
+        public IndividualProceeding IndividualProceeding { get; set; } = null!;
+        public MedicalRecord MedicalRecord { get; set; } = null!;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="individualProceeding"></param>
+        /// <param name="medicalRecord"></param>
+        public InvidiualProceedingWithMedicalRecord(IndividualProceeding individualProceeding, MedicalRecord medicalRecord)
+        {
+            IndividualProceeding = individualProceeding;
+            MedicalRecord = medicalRecord;
+        }
+
+        public InvidiualProceedingWithMedicalRecord()
+        {
+        }
+    }
+}
---
+++ Application/Features/VeterinaryManager/Command/CheckMedicalRecordRequest.cs
@@ -0,0 +1,61 @@
+﻿using Application.DTOs.VeterinaryManager;
+using Application.Managers.Abstraction;
+using Ardalis.GuardClauses;
+using Crosscuting.Api.DTOs;
+using Crosscuting.Api.DTOs.Response;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Features.VeterinaryManager.Command
+{
+    public class CheckMedicalRecordRequest : IRequest<ApiResponse<InvidiualProceedingWithMedicalRecord>>
+    {
+        public Guid IndividualProceedingId { get; private set; }
+        public Guid MedicalRecordId { get; private set; }
+        public AdminData AdminData { get; private set; } = null!;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="individualProceedingId"></param>
+        /// <param name="medicalRecordId"></param>
+        public CheckMedicalRecordRequest(Guid individualProceedingId, Guid medicalRecordId, AdminData adminData)
+        {
+            IndividualProceedingId = individualProceedingId;
+            MedicalRecordId = medicalRecordId;
+            AdminData = adminData;
+        }
+
+    }
+
+    public class CheckMedicalRecordRequestHandler : IRequestHandler<CheckMedicalRecordRequest, ApiResponse<InvidiualProceedingWithMedicalRecord>>
+    {
+        private readonly ILogger<CheckMedicalRecordRequestHandler> Logger;
+        private readonly IVeterinaryManager VeteriyaryManager;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="veteriyaryManager"></param>
+        public CheckMedicalRecordRequestHandler(ILogger<CheckMedicalRecordRequestHandler> logger, IVeterinaryManager veteriyaryManager)
+        {
+            Logger = logger;
+            VeteriyaryManager = veteriyaryManager;
+        }
+
+        ///<inheritdoc />
+        public async Task<ApiResponse<InvidiualProceedingWithMedicalRecord>> Handle(CheckMedicalRecordRequest request, CancellationToken cancellationToken)
+        {
+            Logger.LogInformation("CheckMedicalRecordRequestHandler --> CheckAsync --> Start");
+
+            Guard.Against.Null(request, nameof(request));
+
+            var result = await VeteriyaryManager.CheckMedicalRecord(request.IndividualProceedingId, request.MedicalRecordId, request.AdminData, cancellationToken);
+
+            Logger.LogInformation("CheckMedicalRecordRequestHandler --> CheckAsync --> End");
+
+            return new ApiResponse<InvidiualProceedingWithMedicalRecord>(result);
+        }
+    }
+}
