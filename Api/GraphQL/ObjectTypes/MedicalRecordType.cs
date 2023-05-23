5171aeaffa3824345d81583119fa06684eafd35c
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2023-05-23 21:41:41 +0200

    Implement hotchocolate MedicalRecord type and input type.

---
+++ Api/GraphQL/ObjectTypes/MedicalRecordType.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+
+namespace Api.GraphQL.ObjectTypes
+{
+    public class MedicalRecordType : ObjectType<MedicalRecord>
+    {
+        protected override void Configure(IObjectTypeDescriptor<MedicalRecord> descriptor)
+        {
+            descriptor.Ignore(f => f.IndividualProceedingId);
+            descriptor.Ignore(f => f.MedicalStatusId);
+
+        }
+    }
+}
