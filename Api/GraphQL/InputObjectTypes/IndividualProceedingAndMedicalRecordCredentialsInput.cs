07b4309b0eaa9f090a713f0d62762001a5d3c61d
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2023-05-23 21:41:57 +0200

    Implement input type for ids.

---
+++ Api/GraphQL/InputObjectTypes/IndividualProceedingAndMedicalRecordCredentialsInput.cs
@@ -0,0 +1,16 @@
+﻿using Application.DTOs.VeterinaryManager;
+
+namespace Api.GraphQL.InputObjectTypes
+{
+    public class IndividualProceedingAndMedicalRecordCredentialsInput : InputObjectType<IndividualProceedingAndMedicalRecordCredentials>
+    {
+        protected override void Configure(IInputObjectTypeDescriptor<IndividualProceedingAndMedicalRecordCredentials> descriptor)
+        {
+            descriptor.Field(f => f.IndividualProceedingId)
+                .Type<NonNullType<UuidType>>();
+
+            descriptor.Field(f => f.MedicalRecordId)
+                .Type<NonNullType<UuidType>>();
+        }
+    }
+}
