6c1b6858cd49ab9f5ef9638eb4fe0d436a33cb59
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2023-05-30 00:19:26 +0200

    Fix types.

--- Api/GraphQL/ObjectTypes/MedicalRecordType.cs
+++ Api/GraphQL/ObjectTypes/MedicalRecordType.cs
@@ -6,6 +6,8 @@ namespace Api.GraphQL.ObjectTypes
     {
         protected override void Configure(IObjectTypeDescriptor<MedicalRecord> descriptor)
         {
+            descriptor.Field(f => f.MedicalRecordStatus).Type<NonNullType<MedicalRecordStatusType>>();
+
             descriptor.Ignore(f => f.IndividualProceedingId);
             descriptor.Ignore(f => f.MedicalStatusId);
 
