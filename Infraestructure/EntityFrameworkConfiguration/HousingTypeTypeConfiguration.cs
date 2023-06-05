4404c61db70286d9998a0140750098e810e82b47
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2023-06-05 03:40:45 +0200

    Seed database housing types table. Create hotchocolate enum type.

--- Infraestructure/EntityFrameworkConfiguration/HousingTypeTypeConfiguration.cs
+++ Infraestructure/EntityFrameworkConfiguration/HousingTypeTypeConfiguration.cs
@@ -1,4 +1,5 @@
 using Domain.Entities.Adoption;
+using Domain.Enums.Adoption;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -12,5 +13,14 @@ public class HousingTypeTypeConfiguration : IEntityTypeConfiguration<HousingType
             .ToTable("HousingType", "Adoption")
             .HasKey(x => x.Id)
             .IsClustered(false);
+
+        builder.HasData(Enum.GetValues(typeof(HousingTypes))
+            .Cast<HousingTypes>()
+            .Select(e => new HousingType()
+            {
+                Id = (int)e,
+                Type = e.ToString()
+            })
+        );
     }
 }
